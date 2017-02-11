aef78345bf15fbe71ce725f5ce95cbbdcb14839c
vikashdev <vikash087@gmail.com>
2017-02-12 01:33:17 +0530

    some more php-example

---
+++ php-use/php-sandbox/multiple.php
@@ -0,0 +1,28 @@
+<?php
+// $sum = 0;
+//  $var = $mul = readline("enter no : ")
+// for(; $var > 0 ; $var--) {
+//     if($var != 0 ) {
+//         $sum = $sum + $mul;      
+//     } 
+// }
+// echo  "$sum\n";
+
+// $var1 = readline('enter No : ');
+// $var2 = readline('enter 2nd No :');
+// echo $sum = $var1 + $var2;
+
+
+// echo $sum = (readline('enter 1st No : ')) * (readline('enter 2st No : '));
+
+// prime no
+
+// $num = readline("Enter No");
+// for()
+
+$var1 = 2343234543234565432123456789098765432345;
+$var2 = 2343234543234565432123456789098765432345;
+
+echo $var1 + $var2;
+
+?>
---
+++ php-use/php-sandbox/number-check.php
@@ -0,0 +1,21 @@
+<?php
+
+    //while(($num = readline() )!== '42') {
+      //  echo "$num\n";
+   // }
+
+    // for(; ($num = readline()) !== 42 ; ) {
+    //     if($num == 42) {
+    //          echo "$num\n";
+    //          exit;
+    //     } else {
+    //          echo "$num\n";
+    //     }
+    // }
+
+    for(; ($num = readline()) != 42 ;) {
+        echo "$num\n";
+    }
+
+
+?>
